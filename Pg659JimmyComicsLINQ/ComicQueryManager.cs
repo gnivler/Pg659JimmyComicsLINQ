a29727bbc16a3bb1f0dbe194433e0bce2b969665
gnivler <gnivil@gmail.com>
2016-08-05 14:16:08 -0600

    completed

--- Pg659JimmyComicsLINQ/ComicQueryManager.cs
+++ Pg659JimmyComicsLINQ/ComicQueryManager.cs
@@ -24,6 +24,9 @@ namespace Pg659JimmyComicsLINQ
             {
                 new ComicQuery("LINQ makes queries easy", "A sample query", "Let's show Jimmy how flexible LINQ is", "Assets/purple_250x250.jpg"),
                 new ComicQuery("Expensive comics", "Comics over $500", "Comics whose value is over $500." + " Jimmy can use this to figure out which comics are most coveted.", "Assets/captain_amazing_250x250.jpg"),
+                new ComicQuery("LINQ is versatile 1", "Modify every item returned from the query", "This code will add a string onto the end of each string in an array.", "Assets/bluegray_250x250.jpg"),
+                new ComicQuery("LINQ is versatile 2", "Perform calculations on collections", "LINQ provides extension methods for your collections (and anything else" + " that implements IEnumerable<T>).", "Assets/purple_250x250.jpg"),
+                new ComicQuery("LINQ is versatile 3", "Store all or part of your results in a new sequence", "Sometimes you'll want to keep your results from a LINQ query around.", "Assets/bluegray_250x250.jpg"),
             };
         }
 
@@ -36,6 +39,12 @@ namespace Pg659JimmyComicsLINQ
                     LinqMakesQueriesEasy(); break;
                 case "Expensive comics":
                     ExpensiveComics(); break;
+                case "LINQ is versatile 1":
+                    LinqIsVersatile1(); break;
+                case "LINQ is versatile 2":
+                    LinqIsVersatile2(); break;
+                case "LINQ is versatile 3":
+                    LinqIsVersatile3(); break;
             }
         }
 
@@ -51,10 +60,10 @@ namespace Pg659JimmyComicsLINQ
             {
                 CurrentQueryResults.Add(
                     new
-                        {
-                            Title = i.ToString(),
-                            ImagePath = "Assets/purple_250x250.jpg",
-                        }
+                    {
+                        Title = i.ToString(),
+                        ImagePath = "Assets/purple_250x250.jpg",
+                    }
                     );
             }
         }
@@ -79,6 +88,63 @@ namespace Pg659JimmyComicsLINQ
                     }
                 );
             }
+        }
+
+        private void LinqIsVersatile1()
+        {
+            string[] sandwiches = { "ham and cheese", "salami with mayo", "turkey and swiss", "chicken cutlet" };
+            var sandwichesOnRye = from sandwich in sandwiches
+                                  select sandwich + " on rye";
+
+            foreach (var sandwich in sandwichesOnRye)
+            {
+                CurrentQueryResults.Add(CreateAnonymousListViewItem(sandwich, "bluegray_250x250.jpg"));
+            }
+        }
+
+        private void LinqIsVersatile2()
+        {
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            int length = random.Next(50, 150);
+            for (int i = 0; i < length; i++)
+            {
+                listOfNumbers.Add(random.Next(100));
+
+            }
+
+            CurrentQueryResults.Add(CreateAnonymousListViewItem(string.Format($"There are {listOfNumbers.Count()}")));
+            CurrentQueryResults.Add(CreateAnonymousListViewItem(string.Format($"The smallest is {listOfNumbers.Min()}")));
+            CurrentQueryResults.Add(CreateAnonymousListViewItem(string.Format($"The biggest is {listOfNumbers.Max()}")));
+            CurrentQueryResults.Add(CreateAnonymousListViewItem(string.Format($"The sum is {listOfNumbers.Sum()}")));
+            CurrentQueryResults.Add(CreateAnonymousListViewItem(string.Format($"The average is {listOfNumbers.Average():F2}")));
+        }
+
+        private void LinqIsVersatile3()
+        {
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 1; i <= 10000; i++)
+            {
+                listOfNumbers.Add(i);
+            }
+
+            var under50sorted = from number in listOfNumbers
+                                where number < 50
+                                orderby number descending
+                                select number;
+
+            var firstFive = under50sorted.Take(6);
+
+            List<int> shortList = firstFive.ToList();
+            foreach (int n in shortList)
+            {
+                CurrentQueryResults.Add(CreateAnonymousListViewItem(n.ToString(), "bluegray_250x250.jpg"));
+            }
+        }
+
+        private object CreateAnonymousListViewItem(string title, string imageFilename = "purple_250x250.jpg")
+        {
+            return new { Title = title, ImagePath = $"Assets/{imageFilename}" };
         }
 
         private void CreateImageFromAssets() { }
